34b7b7fd4c4adf6578e870bd0cd65de1ce18612e
Corentinrmt <corentin.remot@gmail.com>
2026-04-02 11:20:09 +0200

    [ADD] exo async await slider + import naughty attributes package

---
+++ Assets/Scripts/LoadingAsync/UI_LoadingAsync.cs
@@ -0,0 +1,84 @@
+using UnityEngine;
+using UnityEngine.UI;
+using NaughtyAttributes;
+using Cysharp.Threading.Tasks;
+using System.Threading.Tasks;
+using System.Threading;
+
+public class UI_LoadingAsync : MonoBehaviour
+{
+    [Header("Slider")]
+    [SerializeField] private Slider _slider;
+    [SerializeField] private Image _backgroundImg;
+    [SerializeField] private Image _fillImg;
+
+    [SerializeField] private Color _fillColor;
+    [SerializeField] private Color _backgroundColor;
+
+    [SerializeField] private float _timeToFill = 5f;
+
+    CancellationTokenSource _cts;
+
+    private void Start()
+    {
+        InitSlider();
+    }
+
+    private void InitSlider()
+    {
+        _fillImg.color = _fillColor;
+        _backgroundImg.color = _backgroundColor;
+
+        _slider.value = 0f;
+    }
+
+    [Button]
+    private void StartLoading()
+    {
+        _cts = new CancellationTokenSource();
+
+        CancellationToken ct = _cts.Token;
+        CancellationToken ct2 = this.destroyCancellationToken;
+
+        CancellationToken combinedCancellationToken = CancellationTokenSource.CreateLinkedTokenSource(ct, ct2).Token;
+
+        _ = LoadingTask(combinedCancellationToken);
+    }
+
+    [Button]
+    private void CancelLoading()
+    {
+        if (_cts != null)
+        {
+            _cts.Cancel();
+        }
+    }
+
+    private async UniTask LoadingTask(CancellationToken ct)
+    {
+        float progress = 0f;
+        bool cancelled = false;
+
+        while (progress < 1f && !cancelled)
+        {
+            progress += Time.deltaTime / _timeToFill;
+            _slider.value = progress;
+            cancelled = await UniTask.Yield(PlayerLoopTiming.Update, ct).SuppressCancellationThrow();
+        }
+
+        if (cancelled)
+        {
+            if (_slider != null)
+            {
+                _slider.value = 0f;
+            }
+        }
+        else
+        {
+            _slider.value = 1f;
+        }
+
+
+
+    }
+}
