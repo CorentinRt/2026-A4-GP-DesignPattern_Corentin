87241b5444353ced84e75af00a03c054fb9ded10
Corentinrmt <corentin.remot@gmail.com>
2026-04-03 12:01:52 +0200

    [ADD]exo linqs

--- Assets/_Linq/LinqPlayground.cs
+++ Assets/_Linq/LinqPlayground.cs
@@ -13,11 +13,32 @@ public class LinqPlayground : MonoBehaviour
     [Button]
     void Run()
     {
-        var pokedex = LoadJson();
+        List<PokemonDto> pokedex = LoadJson();
         Debug.Log("Pokedex loaded");
-        
-        
 
+
+        Debug.Log("Count Psychic : " + pokedex.Where(i => i.Type.Contains("Psychic")).Count());
+
+        /*
+        List<PokemonDto> electricPokemon = pokedex.Where(i => i.Type.Contains("Electric")).ToList();
+
+        PokemonDto pokemon = electricPokemon.Aggregate((a, b) => GetHeight(a) > GetHeight(b) ? a : b);
+
+        Debug.Log("Height : " + pokemon.Name.English);
+        */
+
+        List<PokemonDto> top10 = pokedex.OrderByDescending(i => i.Base.Attack + i.Base.Defense).Take(10).ToList();
+
+        for (int i = 0; i < top10.Count; ++i)
+        {
+            PokemonDto pokemonTop = top10[i];
+            Debug.Log($"top {i + 1} : " + pokemonTop.Name.French);
+        }
     }
     
+    private float GetHeight(PokemonDto pokemon)
+    {
+        return float.Parse(pokemon.Profile.Height.Split(' ')[0]);
+    }
+
 }
